e70ebed18552ccc103e6886400eb3b4e8c824c76
ulissesbon <ulisses.bonfim06@aluno.ifce.edu.br>
2024-11-04 14:22:31 -0300

    add: estudo de conexão back e frontend; manipulação de dados para o backend

--- public/index.js
+++ public/index.js
@@ -11,6 +11,9 @@ async function carregarAnimais(){
 
         //lista que está associada ao id cadastrado no documento html
     const lista = document.getElementById('lista-animais')
+
+    //antes de listar, apago o texto para não ser duplicado em manipularFormulario
+    lista.innerHTML = ''
     
     animais.forEach(animal => {
         //para cada animal eu crio um elemento com o nome e coloco na lista
@@ -19,14 +22,35 @@ async function carregarAnimais(){
 
         lista.appendChild(item)
     });
-
-    
-
 }
+
+
+function manipularFormulario() {
+    const form_animal = document.getElementById('form-animal')
+    const input_nome = document.getElementById('nome')
+
+    form_animal.onsubmit = async (event) => {
+        event.preventDefault() //para não reiniciar a página toda vez que clicar em gravar
+        const nome_animal = input_nome.value
+
+        await axios.post('http://localhost:8000/animais', {
+            nome: nome_animal,
+            idade: 1,
+            sexo: 'Não especificado',
+            cor_pelagem: 'Sem cor'
+        }) 
+
+        alert(`Animal cadastrado: ${nome_animal}`)
+        carregarAnimais()
+    }
+}
+
+
 
 function app(){
     console.log('App iniciado')
     carregarAnimais()
+    manipularFormulario()
 }
 
 app()
