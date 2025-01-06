6dd1c742b5035cf0d130ff324a6d522e0c6de51a
ulissesbon <ulisses.bonfim06@aluno.ifce.edu.br>
2025-01-06 17:17:20 -0300

    fix: rotas_produtos.py

--- olx/src/infra/sqlalchemy/repositorios/repositorio_produto.py
+++ olx/src/infra/sqlalchemy/repositorios/repositorio_produto.py
@@ -12,7 +12,7 @@ class RepositorioProduto():
 
 
     def criar(self, produto: schemas.Produto): # pega o schema do produto e transforma para o banco de dados
-        db_produto = models.Pedido(
+        db_produto = models.Produto(
             nome = produto.nome,
             descricao = produto.descricao,
             preco = produto.preco,
@@ -26,19 +26,19 @@ class RepositorioProduto():
 
 
     def listar(self):
-        produtos = self.db.query(models.Pedido).all()
+        produtos = self.db.query(models.Produto).all()
         return produtos
 
 
     def obter(self, produto_id: int):
-        stmt = select(models.Pedido).filter_by(id=produto_id)
+        stmt = select(models.Produto).filter_by(id=produto_id)
         produto = self.db.execute(stmt).scalar()
         print(produto)
         return produto
 
 
     def remover(self, produto_id: int):
-        stmt = delete(models.Pedido).where(models.Pedido.id == produto_id)
+        stmt = delete(models.Produto).where(models.Produto.id == produto_id)
 
         self.db.execute(stmt)
         self.db.commit()
@@ -46,12 +46,12 @@ class RepositorioProduto():
 
     def editar(self, id_produto: int, produto: schemas.Produto):
         
-        produto_existente = self.db.query(models.Pedido).filter(models.Pedido.id == produto.id).first()
+        produto_existente = self.db.query(models.Produto).filter(models.Produto.id == produto.id).first()
         if not produto_existente:
             raise HTTPException(status_code=404, detail= "Produto não encontrado")
         
-        update_stmt = update(models.Pedido
-                             ).where(models.Pedido.id == id_produto
+        update_stmt = update(models.Produto
+                             ).where(models.Produto.id == id_produto
                                      ).values(
                                             nome = produto.nome,
                                             descricao = produto.descricao,
