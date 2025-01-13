5bbb890dd96256be571b25de07589d9f1f9e1bce
ulissesbon <ulisses.bonfim06@aluno.ifce.edu.br>
2025-01-13 11:47:28 -0300

    add: requirements.txt

--- server.py
+++ server.py
@@ -1,16 +1,14 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.infra.sqlalchemy.config.database import  get_db
 from src.routers import rotas_produtos, rotas_pedidos, rotas_auth, rotas_usuario
 
 
-
 app = FastAPI()
 
 
-
 # CORS
-
 origins = ['http://localhost:3000',
             'https://myapp.vercel.com']
 
@@ -20,7 +18,6 @@ app.add_middleware(CORSMiddleware,
                     allow_methods=["*"],
                     allow_headers=["*"],
                     )
-
 
 
 # ROTAS PRODUTOS
@@ -34,3 +31,8 @@ app.include_router(rotas_usuario.router)
 
 # ROTAS PEDIDOS
 app.include_router(rotas_pedidos.router)
+
+
+
+if __name__ == '__main__':
+    uvicorn.run('server:app', host='0.0.0.0', port=8000, reload=True)
